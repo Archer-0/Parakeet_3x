59662a98c54244d86df93eed88911ae165c60267
Archer <matt76phone@gmail.com>
2018-12-22 19:01:12 +0100

    - UI changes - icon changed - added version check in about page

--- Parakeet_3.0/Parakeet_3.0/About.xaml.cs
+++ Parakeet_3.0/Parakeet_3.0/About.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -15,16 +16,20 @@ using Windows.UI.Xaml.Navigation;
 
 // Il modello di elemento Pagina vuota è documentato all'indirizzo https://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace Parakeet_3._0
-{
+namespace Parakeet_3._0 {
     /// <summary>
     /// Pagina vuota che può essere usata autonomamente oppure per l'esplorazione all'interno di un frame.
     /// </summary>
-    public sealed partial class About : Page
-    {
-        public About()
-        {
+    public sealed partial class About : Page {
+
+        public About() {
             this.InitializeComponent();
+            Package package = Package.Current;
+            PackageId packageId = package.Id;
+            PackageVersion version = packageId.Version;
+
+            string versionStr = string.Format("{0}.{1}.{2}.{3}", version.Major, version.Minor, version.Build, version.Revision);
+            this.AppVersionText.Text = $"Version: {versionStr}";
         }
     }
 }
